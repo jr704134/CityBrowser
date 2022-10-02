79aeacbb231c49b233c0b83fbf693bcb5fa591eb
John Ramirez <businessjuando@gmail.com>
2022-10-01 20:14:05 -0500

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, View, Image } from 'react-native';
+
+const art = require('./Images/art.png');
+const mile = require('./Images/mile.png');
+const pier = require('./Images/pier.png');
+const water = require('./Images/water.png');
+const willis = require('./Images/willis.png');
+
+function ArtScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image source={art} style={styles.picture}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function MileScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image source={mile} style={styles.picture}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function PierScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image source={pier} style={styles.picture}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function WaterScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image source={water} style={styles.picture}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function WillisScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image source={willis} style={styles.picture}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Drawer.Navigator screenOptions={{
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+          <Drawer.Screen name="Art Institute of Chicago" component={ArtScreen} />
+          <Drawer.Screen name="Magnificent Mile" component={MileScreen} />
+          <Drawer.Screen name="Navy Pier" component={PierScreen} />
+          <Drawer.Screen name="Water Tower" component={WaterScreen} />
+          <Drawer.Screen name="Willis Tower" component={WillisScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
   );
 }
 
@@ -16,5 +81,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  picture: {
+    width: 320,
+    height: 480,
   },
 });
